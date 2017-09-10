c16c25058e87bd8029da645f03904aaa043ac533
Emmanuel Osimosu <emmanuel.osimosu@monocl.com>
2017-09-10 03:25:55 +0200

    initial commit

---
+++ consul-watch-handler.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import httplib
+import json
+import sys
+
+SLACK_HOST = ''
+SLACK_PATH = ''
+HEADERS = {'Content-Type': 'application/json'}
+APP_ENVIRONMENT = '{{APP_ENVIRONMENT}}'
+SLACK_CHANNEL = 'consul-alerts'
+COLORS = {'warning': 'warning', 'critical': 'danger', 'passing': 'good'}
+
+
+def construct_payload(check):
+    return {
+        'channel': SLACK_CHANNEL,
+        'username':  APP_ENVIRONMENTg,
+        'icon_emoji': ':microscope:',
+        'attachments': [{
+            'fallback': 'Consul alert - Service: ' + check['ServiceName'] + ', Check: ' + check['Name'] + ', Status: ' +
+                        check[
+                            'Status'] + '.',
+            'color': COLORS[check['Status']],
+            'text': 'Consul alert -',
+            'fields': [
+                {
+                    'title': 'Service',
+                    'value': check['ServiceName'],
+                    'short': True
+                },
+                {
+                    'title': 'Check',
+                    'value': check['Name'],
+                    'short': True
+                },
+                {
+                    'title': 'Node',
+                    'value': check['Node'],
+                    'short': True
+                },
+                {
+                    'title': 'Status',
+                    'value': check['Status'],
+                    'short': True
+                }
+            ]
+        }]
+    }
+
+
+def alert_slack(payload):
+    conn = httplib.HTTPSConnection(SLACK_HOST)
+    conn.request('POST', SLACK_PATH, json.dumps(payload), HEADERS)
+    response = conn.getresponse()
+    conn.close()
+    return response
+
+
+def main():
+    checks = json.loads(sys.stdin.readlines()[0])
+    for check in checks:
+        payload = construct_payload(check)
+        response = alert_slack(payload)
+        print response.status, response.reason
+
+
+if __name__ == "__main__":
+    main()
